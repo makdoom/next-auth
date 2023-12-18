95ecd0da9e79189d46448bb17e2c3004869b6e21
Makdoom Shaikh <makshaikh99@gmail.com>
2023-12-18 21:53:34 +0530

    feat: add db connection with mongoose

---
+++ db/index.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+export const connect = async () => {
+  try {
+    mongoose.connect(process.env.MONGO_URI!);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log("MongoDB connected successfully 🚀");
+    });
+
+    connection.on("error", (error) => {
+      console.log(
+        `MongoDb connection error, Please make sure MongoDb up and running 🤕. ${error}`
+      );
+      process.exit();
+    });
+  } catch (error) {
+    console.log("Something went wrong while connecting to Mongodb 🤕");
+    console.log(error);
+  }
+};
