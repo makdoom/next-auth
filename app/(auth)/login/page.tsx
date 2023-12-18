e357aeb8e62762d25f695115c91f44f698e35475
Makdoom Shaikh <makshaikh99@gmail.com>
2023-12-18 22:52:50 +0530

    feat: add some shadcn component and login/register page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,4 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+
 const Login = () => {
-  return <div>Login Page</div>;
+  return (
+    <div className="h-full w-full flex justify-center items-center flex-1">
+      <div className="border border-[#eee] p-3 md:p-8 w-[350px] flex flex-col justify-center items-center rounded-md">
+        <h2 className="text-center font-bold mb-10 text-3xl tracking-wide text-gray-600">
+          Login
+        </h2>
+        <Input
+          name="email"
+          type="email"
+          autoFocus
+          placeholder="Enter Email"
+          className="w-full"
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Enter Password"
+          className="w-full my-5"
+        />
+        <Button variant="default" className="mt-4 w-full">
+          Register
+        </Button>
+
+        <h2 className="text-sm mt-4">
+          Create new account ?{" "}
+          <Link href="/register" className="font-semibold">
+            Register
+          </Link>
+        </h2>
+      </div>
+    </div>
+  );
 };
 export default Login;
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,4 +1,52 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+
 const Register = () => {
-  return <div>Register Page</div>;
+  return (
+    <div className="h-full w-full flex justify-center items-center flex-1">
+      <div className="border border-[#eee] p-3 md:p-8 w-[350px] flex flex-col justify-center items-center rounded-md">
+        <h2 className="text-center font-bold mb-10 text-3xl tracking-wide text-gray-600">
+          Register
+        </h2>
+        <Input
+          name="name"
+          type="text"
+          autoFocus
+          placeholder="Username"
+          className="w-full"
+        />
+        <Input
+          name="email"
+          type="email"
+          placeholder="Email"
+          className="w-full mt-5"
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Password"
+          className="w-full mt-5"
+        />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm Password"
+          className="w-full my-5"
+        />
+
+        <Button variant="default" className="mt-4 w-full">
+          Register
+        </Button>
+
+        <h2 className="text-sm mt-4">
+          Already have an account ?{" "}
+          <Link href="/login" className="font-semibold">
+            Login
+          </Link>
+        </h2>
+      </div>
+    </div>
+  );
 };
 export default Register;
