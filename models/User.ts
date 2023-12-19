a8d7319b5daa7f7c4802386de5092d260eea0a28
Makdoom Shaikh <makshaikh99@gmail.com>
2023-12-19 21:15:01 +0530

    feat: Add user model

---
+++ models/User.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: [true, "Please enter username"],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please provide an email"],
+  },
+  password: {
+    type: String,
+    unique: true,
+    required: [true, "Please provide password"],
+  },
+});
+
+const User = mongoose.models.users || mongoose.model("User", userSchema);
+
+export default User;
